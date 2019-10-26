1c8677f37c347ecc4585cf64daee4ffdd2877522
gomesjeise <gomes_jeise@hotmail.com>
2019-10-26 16:52:10 -0300

    1. Ajustes dos endpoint; 2. Criacao do pkg Utils e insercao de ENUM de erros.

--- backend/errorcenter/src/main/java/br/com/codenation/errorcenter/repository/UserRepository.java
+++ backend/errorcenter/src/main/java/br/com/codenation/errorcenter/repository/UserRepository.java
@@ -3,10 +3,12 @@ package br.com.codenation.errorcenter.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.codenation.errorcenter.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
-	public Optional<User> findByName(String name);
+	public Optional<User> findByEmail(String email);
 }
