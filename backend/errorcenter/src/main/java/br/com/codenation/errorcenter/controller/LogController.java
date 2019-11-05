20d09ce576b508f882c55ddfac720071e9f87ead
Erica Almeida <erica.jesus@fatec.sp.gov.br>
2019-11-05 18:16:34 -0300

    feat: implementacao do change-log e findByIdLog

--- backend/errorcenter/src/main/java/br/com/codenation/errorcenter/controller/LogController.java
+++ backend/errorcenter/src/main/java/br/com/codenation/errorcenter/controller/LogController.java
@@ -1,11 +1,16 @@
 package br.com.codenation.errorcenter.controller;
 
+import br.com.codenation.errorcenter.dtos.LogDTO;
 import br.com.codenation.errorcenter.models.Log;
 import br.com.codenation.errorcenter.service.LogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
@@ -14,19 +19,26 @@ import javax.servlet.http.HttpServletRequest;
 public class LogController {
     @Autowired
     private LogService logService;
-  
+
     @GetMapping
-    public ResponseEntity<?> getLogs(HttpServletRequest request) throws Exception {
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<?> getLogs() throws Exception {
+        return new ResponseEntity<>(logService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getLogById(HttpServletRequest request, @PathVariable("id") String id) throws Exception {
-        return new ResponseEntity<>(HttpStatus.OK);
+    public Optional<?> getLogById(@PathVariable("id") Long id) {
+        return logService.findById(id); 
     }
 
     @PostMapping
     public ResponseEntity<?> addNewLog(HttpServletRequest request, @RequestBody Log log) throws Exception {
         Log newLog = logService.saveNewLog(log);
         return ResponseEntity.ok(newLog);
+}
+
+    @PatchMapping("/change-status")
+    public ResponseEntity<?> changeStatus(@RequestBody List <LogDTO> log) throws Exception { 
+        logService.update(log);
+        return new ResponseEntity<>(HttpStatus.OK);
+}
 }
---
+++ backend/errorcenter/src/main/java/br/com/codenation/errorcenter/dtos/LogDTO.java
@@ -0,0 +1,23 @@
+package br.com.codenation.errorcenter.dtos;
+
+public class LogDTO {
+    
+    public long id;
+    public String status;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+}
