4638acf639808117b45df5cab2fdf4b6935f53cf
gomesjeise <gomes_jeise@hotmail.com>
2019-11-08 23:37:55 -0300

    Revert "Revert "Merge branch 'master' of https://github.com/codenation-dev/squad-2-ad-java-women-quinto-andar-1""
    This reverts commit 035a70f00f6a0f6695f3206a0b21ea4b5e56bafd.

--- backend/errorcenter/src/main/java/br/com/codenation/errorcenter/controller/LogController.java
+++ backend/errorcenter/src/main/java/br/com/codenation/errorcenter/controller/LogController.java
@@ -1,5 +1,45 @@
 package br.com.codenation.errorcenter.controller;
 
-public class LogController {
 
+import br.com.codenation.errorcenter.dtos.LogRequestDTO;
+import br.com.codenation.errorcenter.models.Log;
+import br.com.codenation.errorcenter.repository.UserRepository;
+import br.com.codenation.errorcenter.service.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/log")
+public class LogController {
+    @Autowired
+    private LogService logService;
+
+    @GetMapping
+    public ResponseEntity<?> getLogs() throws Exception {
+        return new ResponseEntity<>(logService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getLogById(@PathVariable("id") Long id) throws Exception{
+        return new ResponseEntity<>(logService.findById(id), HttpStatus.OK); 
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addNewLog(HttpServletRequest request, @RequestBody Log log) throws Exception {
+        Log newLog = logService.saveNewLog(log);
+        return ResponseEntity.ok("Log inserido com sucesso!");
+    }
+
+    @PatchMapping("/status")
+    public ResponseEntity<?> changeStatus(@RequestBody List <LogRequestDTO> log) throws Exception { 
+        logService.update(log);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
---
+++ backend/errorcenter/src/main/java/br/com/codenation/errorcenter/dtos/UserLoginRequestDto.java
@@ -0,0 +1,6 @@
+package br.com.codenation.errorcenter.dtos;
+
+public class UserLoginRequestDto {
+    public String email;
+    public String password;
+}
