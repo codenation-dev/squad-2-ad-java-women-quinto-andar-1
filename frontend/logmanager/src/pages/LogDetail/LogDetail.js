e746a4af76600a87275b4f3aef2aba14ecd0fe95
Erica Almeida <erica.jesus@fatec.sp.gov.br>
2019-11-23 04:07:15 -0300

    alterações na tela de detalhes do log

--- frontend/logmanager/src/pages/LogDetail/LogDetail.js
+++ frontend/logmanager/src/pages/LogDetail/LogDetail.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import Loader from '../../components/molecules/Loader/Loader';
 import { RequestService } from '../../services/RequestService';
+import './LogDetail.scss';
+
 
 class LogDetail extends Component {
   constructor(props) {
@@ -30,26 +32,33 @@ class LogDetail extends Component {
     const { log } = this.state
 
     return this.state.isAuthenticated
-      ? <div key={log.id} id={log.id}>
-          <div>
-            <p>Título: {log.title}</p>
+      ? <div className="container-detail" key={log.id} id={log.id}>
+        <div className="bottom-link-wrapper">
+          <Link 
+            to="/logs"
+            className="--change"
+            role="button"
+            >Voltar</Link>
+            
           </div>
-          <div>
+        <div className="container-detail-box">
+          <div className="">
+            <p className="title-detail">Erro no {log.origin} em {log.event_date}</p>
+            <p className="container-detail-subtitle">Título <br></br>{log.title}</p>
+            <p className="container-detail-subtitle">Detalhes <br></br></p>
             <p>Descrição: {log.description}</p>
             <p>Origem: {log.origin}</p>
             <p>Ambiente: {log.environment}</p>
             <p>Data do evento: {log.event_date}</p>
-            <p>Coletado por: {log.user_id}</p>
-          </div>
-          <div>
-            <p>Nível: {log.level}</p>
             <p>Status: {log.status}</p>
+
+            <div className="detail-right">
+            <p>{log.level}</p>
+            <p className="container-detail-subtitle">Coletado por: <br></br> {sessionStorage.getItem("tokenAccess")}</p>
           </div>
-          <div>
-            <Link to="/logs">
-              <p>Voltar</p>
-            </Link>
           </div>
+          
+        </div>
         </div>
       : <Loader />
   }
