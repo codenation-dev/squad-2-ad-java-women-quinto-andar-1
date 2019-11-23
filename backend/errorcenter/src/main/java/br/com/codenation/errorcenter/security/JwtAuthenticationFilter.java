e36e589b97a8c722fceb376eee0760a227199921
Marcia Silva <marcia.silva1802@gmail.com>
2019-11-23 03:52:40 -0300

    fix jwt token error handling

--- backend/errorcenter/src/main/java/br/com/codenation/errorcenter/security/JwtAuthenticationFilter.java
+++ backend/errorcenter/src/main/java/br/com/codenation/errorcenter/security/JwtAuthenticationFilter.java
@@ -1,6 +1,10 @@
 package br.com.codenation.errorcenter.security;
 
+import br.com.codenation.errorcenter.exception.MessageException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -10,16 +14,33 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException, ExpiredJwtException {
-        Authentication authentication = JwtToken.validateAuthorizedRequest((HttpServletRequest) request);
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException, ServletException, ExpiredJwtException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        try {
+            Authentication authentication = JwtToken.validateAuthorizedRequest(request);
 
-        filterChain.doFilter(request, response);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException error) {
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            response.getWriter().write(convertObjectToJson(new MessageException(HttpStatus.FORBIDDEN, "Token Expirado", "Não foi possivel realizar a chamada pois os token está expirado")));
+        }
+    }
+
+    public String convertObjectToJson(Object object) throws JsonProcessingException {
+        if (object == null) {
+            return null;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(object);
     }
 }
