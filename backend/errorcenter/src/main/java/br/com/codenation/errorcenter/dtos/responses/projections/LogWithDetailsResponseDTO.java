d470e06c8686879aa73f77bcb239150b06e27a98
Mayara Moromisato <may.alveslima@gmail.com>
2019-11-23 10:18:50 -0300

    Alteracao para recriar o banco.

--- backend/errorcenter/src/main/java/br/com/codenation/errorcenter/dtos/responses/projections/LogWithDetailsResponseDTO.java
+++ backend/errorcenter/src/main/java/br/com/codenation/errorcenter/dtos/responses/projections/LogWithDetailsResponseDTO.java
@@ -3,7 +3,7 @@ package br.com.codenation.errorcenter.dtos.responses.projections;
 import br.com.codenation.errorcenter.models.Environment;
 
 public interface LogWithDetailsResponseDTO extends LogResponseDTO {
-    String getName();
+    String getTokenAccess();
     String getDescription();
     Environment getEnvironment();
 }
